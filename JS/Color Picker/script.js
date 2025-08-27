2d88e59c594e58cba830884a8b1e2a4674af1d90
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-08-27 18:41:37 +0530

    Create script.js

---
+++ JS/Color Picker/script.js
@@ -0,0 +1,4 @@
+function changeColor(color) {
+    document.getElementById('colorPickerContainer').style.backgroundColor = color;
+    document.getElementById('selectedColorHexCode').textContent = color;
+}
