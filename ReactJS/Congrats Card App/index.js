f983883ae9b324de2f5e771116a1a3e1c4bc34a4
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-09-11 17:41:04 +0530

    Create index.js

---
+++ ReactJS/Congrats Card App/index.js
@@ -0,0 +1,14 @@
+const element = (
+  // Write your code here.
+  <div class='bg-container'>
+    <h1>Congratulations</h1>
+    <div class='d-flex flex-column justify-content-center'>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png' />
+      <h1>Kiran Kunta</h1>
+      <p>Vishnu Institute of Computer Education and Technology,Bhimavaram</p>
+      <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
