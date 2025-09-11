6679109754aa4656f71577c3ce4b9e2aef826b6d
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-09-11 17:36:04 +0530

    Create index.js

---
+++ ReactJS/Super Over League/index.js
@@ -0,0 +1,12 @@
+const element = (
+  //   Write your code here.
+  <div>
+    <h1>Super Over League</h1>
+    <div class='image d-flex flex-row justify-content-center'>
+      <img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png' />
+      <img src='https://assets.ccbp.in/frontend/react-js/csk-img.png' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
